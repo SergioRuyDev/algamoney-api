66b248ec352b5c00a26da06836d5f905b135a30b
Sergio Ruy <sergioruyenator@gmail.com>
2020-03-27 09:46:29 -0300

    Initial commit

---
+++ src/main/java/com/sergioruy/algamoneyapi/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.sergioruy.algamoneyapi.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String nome;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        if (getCodigo() != null ? !getCodigo().equals(categoria.getCodigo()) : categoria.getCodigo() != null)
+            return false;
+        return getNome() != null ? getNome().equals(categoria.getNome()) : categoria.getNome() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCodigo() != null ? getCodigo().hashCode() : 0;
+        result = 31 * result + (getNome() != null ? getNome().hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/sergioruy/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.sergioruy.algamoneyapi.repository;
+
+import com.sergioruy.algamoneyapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
