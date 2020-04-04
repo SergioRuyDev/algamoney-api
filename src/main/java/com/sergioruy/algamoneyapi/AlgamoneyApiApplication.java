3c5e4c779d9851d8e1bbe0cd4a8b639d8165df38
Sergio Ruy <sergioruyenator@gmail.com>
2020-04-04 08:54:17 -0300

    Correção do filtro de datas e Class PostConstruct adicionada.

--- src/main/java/com/sergioruy/algamoneyapi/AlgamoneyApiApplication.java
+++ src/main/java/com/sergioruy/algamoneyapi/AlgamoneyApiApplication.java
@@ -3,11 +3,19 @@ package com.sergioruy.algamoneyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
