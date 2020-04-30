30bcc66836722f93ad020965f243a23e5e40d0a3
Sergio Ruy <sergioruyenator@gmail.com>
2020-04-30 15:11:25 +0400

    Api finalizada!

--- src/main/java/com/sergioruy/algamoneyapi/service/LancamentoService.java
+++ src/main/java/com/sergioruy/algamoneyapi/service/LancamentoService.java
@@ -5,6 +5,7 @@ import com.sergioruy.algamoneyapi.model.Pessoa;
 import com.sergioruy.algamoneyapi.repository.LancamentoRepository;
 import com.sergioruy.algamoneyapi.repository.PessoaRepository;
 import com.sergioruy.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,35 @@ public class LancamentoService {
             throw new PessoaInexistenteOuInativaException();
         }
         return lancamentoRepository.save(lancamento);
+    }
+
+    public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+        Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo).get();
+        if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+            validarPessoa(lancamento);
+        }
+
+        BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+        return lancamentoRepository.save(lancamentoSalvo);
+    }
+
+    private void validarPessoa(Lancamento lancamento) {
+        Optional<Pessoa> pessoa = null;
+        if (lancamento.getPessoa().getCodigo() != null) {
+            pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+        }
+
+        if (pessoa==null || !pessoa.isPresent()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+    }
+
+    private Optional<Lancamento> buscarLancamentoExistente(Long codigo) {
+        Optional<Lancamento> lancamentoSalvo = this.lancamentoRepository.findById(codigo);
+        if (!lancamentoSalvo.isPresent() ) {
+            throw new IllegalArgumentException();
+        }
+        return lancamentoSalvo;
     }
 }
